dbd0b15e40dbf6bcc3eff3c4265956b14c451190
sourabh sharma <sourabhsh738@gmail.com>
2026-06-28 22:25:56 +0530

    docs: add swagger documentation for train management apis

--- booking service/src/routes/train.routes.js
+++ booking service/src/routes/train.routes.js
@@ -1,3 +1,18 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: Trains
+ *     description: Train Management APIs
+ */
+
+
+
+
+
+
+
+
+
 const express=require("express")
 const router=express.Router();
 const trainController=require("../controllers/train.controller");
@@ -20,7 +35,62 @@ myBookingSchema
 
 }=require("../validators/booking.validator");
 
+
+/**
+ * @swagger
+ * /trains:
+ *   post:
+ *     summary: Create a new train
+ *     tags: [Trains]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - trainNumber
+ *               - trainName
+ *               - source
+ *               - destination
+ *             properties:
+ *               trainNumber:
+ *                 type: string
+ *                 example: "12951"
+ *               trainName:
+ *                 type: string
+ *                 example: "Rajdhani Express"
+ *               source:
+ *                 type: string
+ *                 example: "NDLS"
+ *               destination:
+ *                 type: string
+ *                 example: "BCT"
+ *     responses:
+ *       201:
+ *         description: Train created successfully
+ *       400:
+ *         description: Invalid request
+ *       409:
+ *         description: Train already exists
+ */
 router.post("/trains",validate(createTrainSchema),trainController.createTrain);
+/**
+ * @swagger
+ * /trains:
+ *   get:
+ *     summary: Get all trains
+ *     description: Returns the complete list of trains.
+ *     tags: [Trains]
+ *
+ *     responses:
+ *
+ *       200:
+ *         description: Train list fetched successfully
+ *
+ *       500:
+ *         description: Internal Server Error
+ */
 router.get("/trains", trainController.getAllTrains);
 router.get("/search-trains",validate(searchTrainSchema,"query"),trainController.searchTrains);
 router.post("/inventory/init",validate(initializeInventorySchema),trainController.initializeInventory);
