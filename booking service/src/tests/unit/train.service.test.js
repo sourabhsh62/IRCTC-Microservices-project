c6135dc15e7e100aa95d2e9796126291b48855a6
sourabh sharma <sourabhsh738@gmail.com>
2026-06-29 18:27:47 +0530

    user not found test case

--- booking service/src/tests/unit/train.service.test.js
+++ booking service/src/tests/unit/train.service.test.js
@@ -117,34 +117,162 @@ describe(
                 client.query
                     .mockResolvedValueOnce();
 
-                 
-                    const result = await bookTicket({
 
-    userId:1,
+                const result = await bookTicket({
 
-    trainId:1,
+                    userId: 1,
 
-    travelDate:"2026-07-10",
+                    trainId: 1,
 
-    seatNumber:10
+                    travelDate: "2026-07-10",
 
-});
+                    seatNumber: 10
 
-expect(result.message)
+                });
 
-.toBe(
+                expect(result.message)
 
-"Ticket booked successfully"
+                    .toBe(
 
-);
+                        "Ticket booked successfully"
+
+                    );
 
 
-expect(acquireLock)
+                expect(acquireLock)
 
-.toHaveBeenCalled();
+                    .toHaveBeenCalled();
+                expect(userBreaker.fire)
+
+                    .toHaveBeenCalledWith(
+
+                        1
+
+                    );
+
+                expect(axios.get)
+
+                    .toHaveBeenCalled();
+
+                expect(pool.connect)
+
+                    .toHaveBeenCalled();
+
+                expect(client.query)
+
+                    .toHaveBeenCalledTimes(
+
+                        5
+
+                    );
+
+                expect(emit)
+
+                    .toHaveBeenCalled();
+
+
+                expect(releaseLock)
+
+                    .toHaveBeenCalled();
+
 
 
             });
 
+            
+test(
+    "should throw error if seat is already booked",
+    async () => {
 
+        acquireLock.mockResolvedValue(true);
+
+        userBreaker.fire.mockResolvedValue({
+            id: 1,
+            email: "test@gmail.com"
+        });
+
+        axios.get.mockResolvedValue({
+            data: {
+                id: 1,
+                trainName: "Rajdhani"
+            }
+        });
+
+        const emit = jest.fn();
+
+        getIO.mockReturnValue({
+            emit
+        });
+
+        const client = {
+            query: jest.fn(),
+            release: jest.fn()
+        };
+
+        pool.connect.mockResolvedValue(client);
+
+        // BEGIN
+        client.query.mockResolvedValueOnce();
+
+        // SELECT Seat
+        client.query.mockResolvedValueOnce({
+            rows: [
+                {
+                    status: "BOOKED"
+                }
+            ]
+        });
+
+        // ROLLBACK
+        client.query.mockResolvedValueOnce();
+
+        await expect(
+            bookTicket({
+                userId: 1,
+                trainId: 1,
+                travelDate: "2026-07-10",
+                seatNumber: 10
+            })
+        ).rejects.toThrow("Seat already booked");
+
+        expect(client.query).toHaveBeenCalledTimes(3);
+
+        expect(releaseLock).toHaveBeenCalled();
+
+        expect(emit).not.toHaveBeenCalled();
+
+    }
+);
+
+
+test(
+    "should throw error if user not found",
+    async () => {
+
+        acquireLock.mockResolvedValue(true);
+
+        // User Service returned null
+        userBreaker.fire.mockResolvedValue(null);
+
+        await expect(
+            bookTicket({
+                userId: 1,
+                trainId: 1,
+                travelDate: "2026-07-10",
+                seatNumber: 10
+            })
+        ).rejects.toThrow("User not found");
+
+        expect(acquireLock).toHaveBeenCalled();
+
+        expect(userBreaker.fire).toHaveBeenCalledWith(1);
+
+        expect(axios.get).not.toHaveBeenCalled();
+
+        expect(pool.connect).not.toHaveBeenCalled();
+
+        expect(releaseLock).not.toHaveBeenCalled();
+
+    }
+);
     });
